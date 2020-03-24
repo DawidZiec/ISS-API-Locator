3771e2dc48d0ccb3fbbf8c77fd4719f05a5bd515
DawidZiec <ziec.dawid@gmail.com>
2020-03-24 15:58:14 +0100

    Added source code

---
+++ iss_api_loc.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import time
+import folium
+from selenium import webdriver
+
+url = "https://api.wheretheiss.at/v1/satellites/25544"
+driver = webdriver.Edge()
+
+
+while(True):
+    response = requests.get(url)
+    response = response.json()
+    os.system('cls')
+    print("Aktualna pozycja ISS")
+    print("Szerokość geograficzna:", response["latitude"])
+    print("Długość geograficzna:", response["longitude"])
+    print("Wysokość:", response["altitude"])
+    lat = response["latitude"]
+    lon = response["longitude"]
+    print()
+
+    m = folium.Map(
+        location=[lat, lon],
+        zoom_start=3
+    )
+
+    folium.Marker([lat, lon], tooltip="ISS").add_to(m)
+
+    m.save('index.html')
+
+    driver.get('index.html')
+    time.sleep(1)
+    driver.refresh()
+
+driver.quit()
+
+    
